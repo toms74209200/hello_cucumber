0b4b73b820bec2814c87d7e555c375c2ee0e054a
toms74209200 <motomotomato@gmail.com>
2021-11-04 23:08:50 +0900

    :sparkles: add first

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # java_hello_cucumber
+
 Cucumber 10 Minute Tutorial
+
+## Ref.
+
+- [10 Minute Tutorial - Cucumber Documentation](https://cucumber.io/docs/guides/10-minute-tutorial/)
---
+++ src/integration-test/java/com/example/hellocucumber/Stepdefs.java
@@ -0,0 +1,33 @@
+package com.example.hellocucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return null;
+    }
+}
+
+public class Stepdefs {
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        today = "Sunday";
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertThat(expectedAnswer).isEqualTo(expectedAnswer);
+    }
+}
